df89ebe2970fc4028eda9b0151f2c740033e363c
ayumi <ayumiprojects@gmail.com>
2023-07-18 16:35:21 -0400

    add photoslider

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 import Header from "./Header";
-
+import DescBanner from "./DescBanner";
 
 export default function App() {
-    return(
-        <Header />
-    )
+  return (
+    <>
+      <Header />
+      <DescBanner />
+    </>
+  );
 }
---
+++ src/components/DescBanner.jsx
@@ -0,0 +1,34 @@
+import PhotoSlider from "./PhotoSlider";
+import bannerImg1 from "../assets/images/banner-img-1.webp";
+import bannerImg2 from "../assets/images/banner-img-2.webp";
+const DescBanner = () => {
+  return (
+    <div className="desc-banner-wrapper">
+      <div className="banner-container">
+        <PhotoSlider image1={bannerImg1} image2={bannerImg2} />
+        <div className="desc-container">
+          <h2>ESCAPE TO TROPICAL TRANQUILITY</h2>
+          <p>
+            Step into our sanctuary for the senses, where the sapphire blue sea
+            meets tropical white sands against a backdrop of lush jungle-green
+            mountains.
+          </p>
+          <p>
+            <br />
+            We welcome you to a heavenly oasis of peace and comfort in our
+            idyllic beachfront resort. Feel the warm embrace of Banyan Tree
+            hospitality - from the tranquil treatments at Banyan Tree Spa to
+            mouthwatering cuisine at our stylish restaurants to the beach bar
+            with its spectacular vistas of the Andaman Sea.
+          </p>
+          <p>
+            <br />
+            Retreat to a setting of unparalleled indulgence where you can truly
+            unwind and rejuvenate your mind, body and soul.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default DescBanner;
