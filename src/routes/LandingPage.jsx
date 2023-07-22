e5dcda2b433c45dc689c84d9d2806c2dd79e1acd
ayumi <ayumiprojects@gmail.com>
2023-07-22 16:28:46 -0400

    change header path

--- src/routes/LandingPage.jsx
+++ src/routes/LandingPage.jsx
@@ -1,4 +1,4 @@
-import Header from "../components/Header";
+import Header from "../components/Header/Header";
 import DescBanner from "../components/DescBanner";
 import InfoSection from "../components/InfoSection";
 const LandingPage = () => {
