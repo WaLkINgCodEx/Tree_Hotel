77a3c20ad51d60816357c48fb5210bc7ba8f5bb9
Walker Au <realcodewalker@gmail.com>
2023-07-30 18:59:55 -0400

    destruct header components, create <NavBarMobile> & <NavBar>

--- src/components/Header/LangMenu.jsx
+++ src/components/Header/LangMenu.jsx
@@ -3,6 +3,7 @@ import Button from "@mui/material/Button";
 import Menu from "@mui/material/Menu";
 import MenuItem from "@mui/material/MenuItem";
 import { styled, alpha } from "@mui/material/styles";
+import BigArrow from "../../assets/icons/BigArrow";
 
 export default function LangMenu(props) {
   const [anchorEl, setAnchorEl] = useState(null);
@@ -75,16 +76,7 @@ export default function LangMenu(props) {
         onClick={handleClick}
       >
         <span>
-          <svg
-            viewBox="0 0 24 24"
-            xmlns="http://www.w3.org/2000/svg"
-            width="22"
-            className="IconArrow-right css-48nvow"
-          >
-            <g fill="none" stroke="currentColor">
-              <path d="M11.889 16.728S13.249 9.433 21.778 7M11.889 16.728S10.529 9.433 2 7"></path>
-            </g>
-          </svg>
+          <BigArrow />
         </span>
         {lang}
       </Button>
---
+++ src/components/Header/NavBar.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import Li from "./Li";
+
+export default function NavBar() {
+  return (
+    <div className="nav-bar-top">
+      <ul>
+        <Li
+          brandIcon="logo"
+          className="icon-logo"
+          img="./assets/images/icon-logo-green-2.svg"
+          alt="icon-logo"
+          // key="logo"
+        />
+        <Li name="ABOUT" url="/" />
+        <Li name="OFFERS" url="offers" />
+        <Li name="ACCOMMODATION" url="accommodation" />
+        <Li name="DINING" url="dining" />
+        <Li name="SPA" url="spa" />
+        <Li name="EXPERIENCES" url="experience" />
+        <Li name="MORE" className="arrow-icon" />
+        <Li name="EN" className="arrow-icon" />
+        <Li name="BOOK NOW" idName="bookNowButton" />
+      </ul>
+    </div>
+  );
+}
